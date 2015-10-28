296be51aa623b3b55fa24dec2195524ff69a4d1b
Hury Gabriel <hury_gabriel@hotmail.com>
2015-10-28 10:24:54 -0200

    Update QueryMasp.sql

--- Projeto_MASP/src/connection/QueryMasp.sql
+++ Projeto_MASP/src/connection/QueryMasp.sql
@@ -22,19 +22,30 @@ primary key(cnpj)
 --Dados dos representantes da empresa
 CREATE TABLE representante(
 
-cnpj varchar(12),
-nome varchar(100),
-cpf varchar(11),
+cnpj varchar(12) not null,
+nome varchar(100) not null,
+cpf varchar(11) not null,
 rg varchar(9),
 tel varchar(11),
 primary key (rg)
 foreign key ( cnpj ) references empresa(cnpj)
 );
 
+--Dados do autor
+CREATE TABLE autor(
+
+codAutor int,
+nome varchar(100) not null,
+dataNasc datetime,
+nacionalidade varchar(50),
+primary key (codAutor)
+)
+
+
 --Dados de uma obra
 CREATE TABLE obra(
 
-autor varchar(100) not null,
+autor int not null,
 titulo varchar(100) not null,
 subtitulo varchar(100),
 edicao varchar(10),
@@ -42,7 +53,13 @@ editor varchar(100),
 ano varchar(4) not null , 
 isbn varchar(13),
 primary key (autor, titulo)
+foreign key ( autor ) references autor(codAutor)
 );
 
+CREATE TABLE pessoa(
+nome varchar(100) not null,
+dataNasc datetime,
+nivelAcad varchar(50) not null
+)
 
 
